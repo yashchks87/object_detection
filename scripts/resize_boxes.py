05deff26057feec9f42c103a0019c696cd24ee7f
yashchks87 <yashchks87@gmail.com>
2023-10-25 19:59:49 +0000

    Anchor box generation is finished.

--- scripts/resize_boxes.py
+++ scripts/resize_boxes.py
@@ -2,13 +2,14 @@ def fix_original_values(box):
     updated_box = [box[0], box[1], box[2]- box[0], box[3]- box[1]]
     return updated_box
 
-def resize_values(box, old_img_size, target_img_size, return_old_box = True, is_torch = False):
+def resize_values(box, old_img_size, target_img_size, fix_original = False, return_old_box = True, is_torch = False):
     if is_torch:
         width, height = old_img_size[2], old_img_size[1]
     else:
         width, height = old_img_size[1], old_img_size[0]
-    old_updated_box = fix_original_values(box)
-    x1, y1, x2, y2 = old_updated_box[0], old_updated_box[1], old_updated_box[2], old_updated_box[3]
+    if fix_original:
+        box = fix_original_values(box)
+    x1, y1, x2, y2 = box[0], box[1], box[2], box[3]
     x1_o = x1 / width
     y1_o = y1 / height
     x2_o = x2 / width
